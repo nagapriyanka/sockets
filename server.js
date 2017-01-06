27127d29296c7dffc2f9c775962d9d385be53eae
priyanka <nagapriyanka.credencys@gmail.com>
2017-01-06 12:42:46 +0530

    Broadcasing message

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ var PORT = process.env.PORT || 3000 ;
 var express = require('express');
 var app = express();
 var http  = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use(express.static(__dirname +'/public'));
+
+io.on('connection',function(socket){
+	console.log("User Connected Via socket.io");
+	socket.on('message',function(message){
+		console.log("Message Received:"+ message.text);
+		socket.broadcast.emit('message',message);
+	})
+	socket.emit('message',{
+		text : 'Welcome to the chat Application !'
+	});
+});
 
 http.listen(PORT , function(){
 	console.log('Server Started');
